7815b7d96a9fc659a8b0849fe7fa44e48ee6c687
flp96 <filipefernandes__@live.com>
2025-07-25 16:11:17 -0300

    foto delete finalizado

--- src/api.jsx
+++ src/api.jsx
@@ -85,7 +85,7 @@ export function PHOTO_GET(id) {
 
 export function COMMENT_POST(id, body) {
   return {
-    url: `${API_URL}/api/photo/${id}`,
+    url: `${API_URL}/api/comment/${id}`,
     options: {
       method: 'POST',
       headers: {
@@ -93,6 +93,19 @@ export function COMMENT_POST(id, body) {
         Authorization: 'Bearer ' + window.localStorage.getItem('token'),
       },
       body: JSON.stringify(body),
+    },
+  };
+}
+
+export function PHOTO_DELETE(id) {
+  return {
+    url: `${API_URL}/api/photo/${id}`,
+    options: {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer ' + window.localStorage.getItem('token'),
+      },
     },
   };
 }
